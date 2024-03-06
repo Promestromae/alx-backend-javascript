e25159ee48ed03d58d52aac888b173ca85fa2e0c
Promestromae <nzuvameshack@gmail.com>
2024-03-06 10:01:05 -0800

    with you lucie

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,3 @@
-// 0-promise.js
-
-function getResponseFromAPI() {
-  // Simulate asynchronous behavior with a short delay
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({ message: "API response" });
-    }, 1000);
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
-
